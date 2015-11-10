d805f8645729c5da222534e7c0fdc896b59144b0
Emanuel Matheus <emanuelmatheusssm@hotmail.com>
2015-11-10 00:48:40 -0200

    first commit
    adding the index page (empty)

---
+++ index.php
@@ -0,0 +1,24 @@
+<head><title>Game Hunter | Home</title><meta charset="utf-8"/></head>
+<?php
+require_once 'gamehunter.class.php';
+?>
+<body>
+    <div id="menu">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="mygames.php">Meus Jogos</a></li>
+                <li><a href="friends.php">Amigos</a></li>
+                <li><a href="locate.php">Localizar</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div id="mostplayed">
+        
+    </div>
+    <div id="gamessujest">
+        
+    </div>
+    <div id="news">
+
+    </div>
