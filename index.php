8ee58f905e7e34adc2048f565a1ba376ee814fff
Makinex <chapinhon5@hotmail.com>
2015-11-10 01:11:31 -0200

    Primeiro layout
    Fiz o seco do que tava no primeiro layout falta as div e class e tals, quem for fazer o css ja bota ai

--- index.php
+++ index.php
@@ -1,24 +1,40 @@
-<head><title>Game Hunter | Home</title><meta charset="utf-8"/></head>
-<?php
-require_once 'gamehunter.class.php';
-?>
-<body>
-    <div id="menu">
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="mygames.php">Meus Jogos</a></li>
-                <li><a href="friends.php">Amigos</a></li>
-                <li><a href="locate.php">Localizar</a></li>
-            </ul>
-        </nav>
-    </div>
-    <div id="mostplayed">
-        
-    </div>
-    <div id="gamessujest">
-        
-    </div>
-    <div id="news">
-
-    </div>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+    </head>
+    <body>
+        <ul>
+            <li><a href="home;php">Home</a></li>
+            <li><a href="meusj.php">Meus Jogos</a></li>
+            <li><a href="amigos.php">Amigos</a></li>
+            <li><a href="perfil.php">Perfil</a></li>
+        </ul>
+        <h1>Bem-vindo!</h1>
+        <h2>Para ter total acesso, por favor faca o <a href="login.php">login</a> com a sua conta do facebook.</h2>
+        <ul>
+            <li><h3>Noticias</h3></li>
+            <li>Nova expansao World of Warcraf Legion e anunciada na BlizzCon.</li>
+            <li>Overwatch ganha edicoes especiais e versoes para PS4 e Xbox One.</li>
+            <li>StarCraft 2: Nova Covert Ops continuara a historia do game em DLC.</li>
+            <li><h5><a href="noticias.php">Ver todas</a></h5></li>
+        </ul>
+        <ul>
+            <li><h3>Ultimos lancamentos</h3></li>
+            <li>Call Of Duty:Black Ops III 
+                <p>Plataformas: Xbox One, PS4, Xbox 360, PS3 e PC</p>
+                <p>Data de lancamento: 06/11/2015</p>
+            </li>
+            <li>Need for Speed 2015 
+                <p>Plataformas: Xbox One, PS4 e PC</p>
+                <p>Data de lancamento: 03/11/2015</p>
+            </li>
+            <li>FIFA 16
+                <p>Plataformas: Xbox One, PS4, Xbox 360, PS3 e PC</p>
+                <p>Data de lancamento: 22/09/2015</p>
+            </li>
+            <li><a href="lanc.php"><h5>Ver todos</h5></a></li>
+        </ul>
+    </body>
+</html>
